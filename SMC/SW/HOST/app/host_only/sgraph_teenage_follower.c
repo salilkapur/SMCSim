4ed17a3dbcd63950edd31ef96f6e90f9e0e90ef3
Erfan Azarkhish <erfan.azarkhish@unibo.it>
2016-11-24 10:18:28 +0100

    sgraph_teenage_follower improved

--- SMC/SW/HOST/app/host_only/sgraph_teenage_follower.c
+++ SMC/SW/HOST/app/host_only/sgraph_teenage_follower.c
@@ -55,52 +55,18 @@ unsigned long run_golden()
 }
 
 /****************************************************************************/
+#include "create_topology.h"
 
 void create_graph()
 {
-    // Initialize the list of lists
-    // Later, we should read the graph from data sets
-    nodes = (node*)malloc(NODES*sizeof(node));
-    assert (nodes);
-    successors_size = 0;
-    successors_list = NULL;
+    create_topology();
 
-    assert(MAX_OUTDEGREE < NODES);
-
+    // Kernel specific initializations
     for ( unsigned long i=0; i<NODES; i++ )
     {
-        unsigned long num_succ = (i%step==0)?(rand()%MAX_OUTDEGREE):(0);
-        nodes[i].out_degree = num_succ;
-        nodes[i].ID = i;
-        if ( num_succ == 0 )
-            nodes[i].successors = NULL;
-        else
-        {
-            nodes[i].successors = (node**)malloc(num_succ*sizeof(node*)); 
-            assert (nodes[i].successors);
-            successors_size += num_succ*sizeof(node*);
-        }
-
-        /* fill the successor list of node[i] */
-        unsigned long succ = rand()%NODES; // Random;
-        assert(MAX_OUTDEGREE < 500);
-        for ( unsigned long j=0; j<num_succ; j++ )
-        {
-            while (succ==i)
-            {
-                succ += (rand()%(NODES/500) + 1);
-                if ( succ >= NODES )
-                    succ -= NODES;
-            }
-            /* we have found a new successor which we are not already connected to */
-            nodes[i].successors[j] = &nodes[succ];
-        }
-
-        // Kernel specific initializations
         nodes[i].teenager = (rand()%2);
     }
 
-    successors_list = nodes[0].successors;
     reset_graph_stats();
 }
 
