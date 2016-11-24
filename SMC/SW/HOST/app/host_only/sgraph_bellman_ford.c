012f18054079acac15ee69bb4c0e258f2af48ade
Erfan Azarkhish <erfan.azarkhish@unibo.it>
2016-11-24 10:30:31 +0100

    sgraph_bellman_ford improved

--- SMC/SW/HOST/app/host_only/sgraph_bellman_ford.c
+++ SMC/SW/HOST/app/host_only/sgraph_bellman_ford.c
@@ -62,54 +62,13 @@ node**  successors_list;     // List of the successors (not used directly)
     }
 
 /****************************************************************************/
-
+#include "create_topology.h"
 
 void create_graph()
 {
-    // Initialize the list of lists
-    // Later, we should read the graph from data sets
-    nodes = (node*)malloc(NODES*sizeof(node));
-    assert (nodes);
-    successors_size = 0;
-    successors_list = NULL;
+    create_topology();
 
-    assert(MAX_OUTDEGREE < NODES);
-
-    for ( unsigned long i=0; i<NODES; i++ )
-    {
-        unsigned long num_succ = (i%step==0)?(rand()%MAX_OUTDEGREE):(0);
-        nodes[i].out_degree = num_succ;
-        nodes[i].ID = i;
-        if ( num_succ == 0 )
-            nodes[i].successors = NULL;
-        else
-        {
-            nodes[i].successors = (node**)malloc(num_succ*sizeof(node*)); 
-            assert (nodes[i].successors);
-            successors_size += num_succ*sizeof(node*);
-        }
-
-        /* fill the successor list of node[i] */
-        unsigned long succ = rand()%NODES; // Random;
-        assert(MAX_OUTDEGREE < 500);
-        for ( unsigned long j=0; j<num_succ; j++ )
-        {
-            while (succ==i)
-            {
-                succ += (rand()%(NODES/500) + 1);
-                if ( succ >= NODES )
-                    succ -= NODES;
-            }
-            /* we have found a new successor which we are not already connected to */
-            nodes[i].successors[j] = &nodes[succ];
-        }
-
-        // Kernel specific initializations
-        #ifdef sgraph_teenage_follower
-        nodes[i].teenager = (rand()%2)?true:false;
-        #endif
-    }
-
+    // Kernel specific initializations
     printf(" Initializing weights_list for the weighted graph ...\n");
     weights_size = 0;
     weights_list = NULL;
@@ -130,8 +89,6 @@ void create_graph()
     }
     
     weights_list = nodes[0].weights;
-
-    successors_list = nodes[0].successors;
     reset_graph_stats();
 }
 
